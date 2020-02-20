68d0ad1765e29af18fab88e8f4869b921241ffd0
john <hszhongtao@163.com>
2020-02-20 14:42:05 +0800

    rabbit发送数据保存请求

--- test/rabbit/topic/consumer.py
+++ test/rabbit/topic/consumer.py
@@ -1,5 +1,7 @@
 import json
 
+import requests
+
 from test.rabbit.topic.rabbit import rabbit_message_process, rabbit_connect
 
 rabbit_connect('192.168.0.105', 5672, 'root', '123456', 'rtz')
@@ -14,4 +16,27 @@ def process_message(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-rabbit_message_process('rtz', 'topic', 'rtz', 'rtz.images', process_message)
+def save(ch, method, properties, body):
+    # 请求rtz接口
+    headers = {
+        'service': 'rtz_0001',
+        'secret': 'bd3273c7a37b5ab33ed3e996d14f744d'
+    }
+    req_data = json.loads(body)
+    res = requests.put('http://127.0.0.1:10002/api/fs/v1/rtz/save', headers=headers, data=req_data).content
+    if not res:
+        print('error %s' % str(body))
+        return None
+
+    res_json = json.loads(res)
+    if not res_json['status']:
+        print('error %s' % str(body))
+        return None
+
+    print(res_json['data'])
+
+    # 发送确认,删除消息
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+rabbit_message_process('rtz', 'topic', 'rtz', 'rtz.images', save)
