12a164f320c510a74604d3d18433e9d6d78bf33f
john <hszhongtao@163.com>
2020-02-20 14:35:26 +0800

    rabbit celery 联调测试

--- test/rabbit/topic/consumer.py
+++ test/rabbit/topic/consumer.py
@@ -1,10 +1,13 @@
+import json
+
 from test.rabbit.topic.rabbit import rabbit_message_process, rabbit_connect
 
 rabbit_connect('192.168.0.105', 5672, 'root', '123456', 'rtz')
 
 
 def process_message(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
+    # print(" [x] %r:%r" % (method.routing_key, body))
+    print('result: %s' % json.loads(body))
 
     # 发送确认,删除消息
     print(method.delivery_tag)
